86f5cd9d8917392d781c6e2fefce95e14f6ce45a
Lucie-Garcia <lucie.garcia05@edu.univ-fcomte.fr>
2026-01-30 09:33:40 +0100

    TP3 commit 2, ajout des images

--- src/backend.mjs
+++ src/backend.mjs
@@ -4,13 +4,16 @@ const db = new PocketBase("http://127.0.0.1:8090");
 
 export async function getOffres() {
     try {
-        const data = await db.collection('Maison').getFullList({
+        let data = await db.collection('Maison').getFullList({
             sort: '-created',
         });
-        console.log(data);
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la liste des maisons', error);
         return [];
     }
+}
+
+export async function getImageUrl(record, recordImage) {
+    return db.files.getURL(record, recordImage);
 }
